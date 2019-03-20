8ca2b49c0162561a698836804f0599cd7ae147b1
Reinald Reynoso <reireynoso@gmail.com>
2019-03-20 18:57:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  #puts data
+  pigeon_list = {}
+  data.each do |category,more_data|
+    if category == :gender
+      more_data.each do |sex,names|
+        names.each do |name|
+          data.each do |category,more_data|
+            
+          end
+        end
+      end
+    end
+  end
+  puts pigeon_list
 end
