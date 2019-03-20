c4e7f826384cbc1ff6db0c1f2a41079d24e1a54f
Reinald Reynoso <reireynoso@gmail.com>
2019-03-20 19:56:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,38 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  #puts data
+  pigeon_list = {}
+  
+  data.each do |category,more_data|
+    if category == :gender
+      more_data.each do |sex,names|
+        names.each do |name|
+          pigeon_list[name] = {}
+        end
+      end
+    end
+  end
+  
+  pigeon_list.each do |name,hash|
+    data.each do |category,more_data|
+      hash[category] = []
+    end
+  end
+  
+  pigeon_list.each do |name,hash|
+    hash.each do |categ,array|
+      data.each do |category,more_data|
+        if category == categ
+          more_data.each do |key,value|
+            value.each do |array1|
+              if array1 == name
+                array << key.to_s
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  return pigeon_list
 end
