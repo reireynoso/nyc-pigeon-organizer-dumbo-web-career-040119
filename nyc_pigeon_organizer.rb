94879a07cc249b4f724f651e9da7fd48dda93299
Reinald Reynoso <reireynoso@gmail.com>
2019-03-20 19:51:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,7 +23,9 @@ def nyc_pigeon_organizer(data)
     hash.each do |categ,array|
       data.each do |category,more_data|
        more_data.each do |key, value|
-         puts key
+         value.each do |array1|
+           puts array1
+         end
        end
       end
     end
