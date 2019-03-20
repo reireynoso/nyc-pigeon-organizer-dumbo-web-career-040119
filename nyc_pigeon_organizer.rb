dd15007f1240cb4744d2ad3129f060686cc2c35d
Reinald Reynoso <reireynoso@gmail.com>
2019-03-20 18:59:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,9 +6,7 @@ def nyc_pigeon_organizer(data)
     if category == :gender
       more_data.each do |sex,names|
         names.each do |name|
-          data.each do |category,more_data|
-            
-          end
+          pigeon_list[name] = {}
         end
       end
     end
